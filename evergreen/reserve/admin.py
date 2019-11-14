667b57b414416e0039222950e6ca02b77664ec49
jaywang-EE <gaaiia37896@gmail.com>
2019-11-13 22:27:21 -0500

    123

--- evergreen/reserve/admin.py
+++ evergreen/reserve/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from reserve.models import Reserve, Table
+from reserve.models import Reserve, Table, TableType
 
 # Register your models here.
 
 admin.site.register(Reserve)
 admin.site.register(Table)
+admin.site.register(TableType)
 
