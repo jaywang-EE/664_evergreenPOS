92431f98671a13253b7e06140b60a12ceae0e22c
jaywang-EE <gaaiia37896@gmail.com>
2019-11-14 14:21:05 -0500

    auto_fillin

--- evergreen/reserve/forms.py
+++ evergreen/reserve/forms.py
@@ -4,23 +4,16 @@ from django import forms
 from .models import Reserve
 
 class CreateForm(forms.ModelForm):
-    def clean_file(self,form):
-        if person<table.min_person:
-            raise ValidationError(_('Invalid value'), code='invalid')
-            #raise ValidationError("Table %02d should have at least %d diners"%(table.number, table.min_person))
-        elif person>table.max_person:
-            raise ValidationError("Table %02d con only contain %d diners"%(table.number, table.max_person))
-
     def clean(self):
-        cleaned_data = super().clean()
+        cleaned_data = super(CreateForm, self).clean()
         table = cleaned_data.get("table")
         person = cleaned_data.get("person")
 
-        if person>table.max_person:
-            raise forms.ValidationError("Table %s con only contain %d diners"%(table.name, table.max_person))
-        if person<table.min_person:
-            raise forms.ValidationError("Table %02d should have at least %d diners"%(table.name, table.min_person))
+        if person>table.category.max_person:
+            raise forms.ValidationError("Table %s con only contain %d diners"%(table, table.category.max_person))
+        if person<table.category.min_person:
+            raise forms.ValidationError("Table %s should have at least %d diners"%(table, table.category.min_person))
 
     class Meta:
         model = Reserve
-        fields = ['custom', 'date', 'hour', 'person', 'table']
+        fields = ['custom', 'date', 'hour', 'person', 'table']#['custom', 'person']#
