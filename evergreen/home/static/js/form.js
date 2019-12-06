dc9e3d4e430b7f2ad7700a42f4b1ec3d970e3328
hsinhu <54917112+hsinhu@users.noreply.github.com>
2019-12-06 14:38:06 -0500

    Update form.js

--- evergreen/home/static/js/form.js
+++ evergreen/home/static/js/form.js
@@ -4,18 +4,22 @@ $(".changenum").submit(function(event) {
   var select = element.querySelector("select").value; 
   var button = element.querySelector("button").value; 
   document.cookie = "meal_id_"+button+"="+select+";path=/";
-  var selectPrice = document.querySelectorAll("#cart h3")
+  var selectPrice = document.querySelectorAll("#cart .s_price")
+  var dish = document.querySelectorAll("#cart h3")
   var selectvalue = document.querySelectorAll("#cart select")
-  var i;
   var totalprice = 0;
   for (i = 0; i < selectPrice.length; i++) {
   	var str = selectPrice[i].innerText
   	var price = str.match(/[+-]?\d+(?:\.\d+)?/).map(Number);
   	var number = selectvalue[i].value
-	totalprice = totalprice + price*number
+    var dishname = dish[i].innerText.split(":")[0];
+    dishprice = number*price
+    dishprice = dishprice.toFixed(2)
+      console.log(dishname)
+    $(dish[i]).html( dishname + ": " + dishprice + "$")
+	  totalprice = totalprice + price*number
   	// console.log(price)
   	// console.log(number)
-
 	}
 	totalprice = totalprice.toFixed(2)
 	// console.log(totalprice)
